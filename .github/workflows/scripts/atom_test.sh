65f8b68ac7116b7967147916a1ad3f8a175b1017
Xin Huang <Xin.Huang@amd.com>
2025-12-23 10:42:03 +0800

    Optimze the atom test scripts

---
+++ .github/workflows/scripts/atom_test.sh
@@ -0,0 +1,43 @@
+set -euo pipefail
+
+TYPE=${1:-launch}
+MODEL_PATH=${2:-meta-llama/Meta-Llama-3-8B-Instruct}
+EXTRA_ARGS=${3:-""}
+
+
+if [ "$TYPE" == "launch" ]; then
+  echo ""
+  echo "========== Launching ATOM server =========="
+  python -m atom.entrypoints.openai_server --model $MODEL_PATH $EXTRA_ARGS
+  atom_server_pid=$!
+
+  echo ""
+  echo "========== Waiting for ATOM server to start =========="
+  max_retries=60
+  retry_interval=1
+  for ((i=1; i<=max_retries; i++)); do
+      if curl -s http://localhost:8000/v1/completions -o /dev/null; then
+          echo "ATOM server is up."
+          break
+      fi
+      echo "Waiting for ATOM server to be ready... ($i/$max_retries)"
+      sleep $retry_interval
+  done
+  if ! curl -s http://localhost:8000/v1/completions -o /dev/null; then
+      echo "ATOM server did not start after $((max_retries * retry_interval)) seconds."
+      kill $atom_server_pid
+      exit 1
+  fi
+fi
+
+if [ "$TYPE" == "accuracy" ]; then
+echo ""
+echo "========== Installing lm-eval =========="
+pip install lm-eval[api]
+echo ""
+echo "========== Running accuracy test =========="
+lm_eval --model local-completions \
+        --model_args model=$MODEL_PATH,base_url=http://localhost:8000/v1/completions,num_concurrent=64,max_retries=3,tokenized_requests=False \
+        --tasks gsm8k \
+        --num_fewshot 3
+fi
