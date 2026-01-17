93975b31e2bc574f6378d485d8e0cc939a545d09
ZhangLirong-amd <Lirong.Zhang@amd.com>
2026-01-17 11:15:19 +0800

    fix qwen 235 kv head 4 core dump

--- atom/model_ops/attentions/aiter_attention.py
+++ atom/model_ops/attentions/aiter_attention.py
@@ -50,7 +50,7 @@ class AiterAttentionMetadataBuilder(CommonAttentionBuilder):
         else:
             max_qlen = 1
 
-        num_head_k = hf_config.num_key_value_heads // get_tp_group().world_size
+        num_head_k = max(1, hf_config.num_key_value_heads // get_tp_group().world_size)
         (
             (work_meta_data_size, work_meta_data_type),
             (work_indptr_size, work_indptr_type),
