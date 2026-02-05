dd0d24dc9fadf9bd7d84bac6a352462821b57591
Guanbao Yu <gyu@amd.com>
2026-02-05 10:44:47 +0800

    fused_rope_rms was deleted from aiter

--- atom/models/qwen3_moe.py
+++ atom/models/qwen3_moe.py
@@ -39,8 +39,6 @@ from atom.models.utils import (
 )
 from atom.utils import envs
 
-from aiter import fused_rope_rms
-
 ENABLE_ALLREDUCE_RMSNORM_FUSION = envs.ATOM_ENABLE_ALLREDUCE_RMSNORM_FUSION
 ATOM_ENABLE_QK_NORM_ROPE_CACHE_QUANT_FUSION = (
     envs.ATOM_ENABLE_QK_NORM_ROPE_CACHE_QUANT_FUSION
@@ -114,21 +112,7 @@ class RotaryEmbeddingQKNormFused(nn.Module):
         num_kv_heads: int,
         eps: float,
     ) -> Tuple[torch.Tensor, torch.Tensor]:
-        num_tokens = positions.shape[-1]
-        fused_rope_rms(
-            qkv,
-            q_weight,
-            k_weight,
-            self.cos_sin_cache,
-            positions,
-            num_tokens=num_tokens,
-            num_heads_q=num_heads,
-            num_heads_k=num_kv_heads,
-            num_heads_v=num_kv_heads,
-            head_size=self.head_size,
-            is_neox_style=self.is_neox_style,
-            eps=eps,
-        )
+        pass
 
 
 class Qwen3MoeMLP(nn.Module):
