b45f9c6770fb9147dbfda925aa3991dc93954a43
Lingpeng Jin <103567126+valarLip@users.noreply.github.com>
2026-03-18 16:02:57 +0000

    fix black

--- atom/model_ops/attention_mla.py
+++ atom/model_ops/attention_mla.py
@@ -37,7 +37,6 @@ from aiter.ops.triton.batched_gemm_a8w8_a_per_token_group_prequant_w_per_batched
     batched_gemm_a8w8_a_per_token_group_prequant_w_per_batched_tensor_quant as _aiter_triton_fp8_bmm,
 )
 
-
 concat_and_cache_mla = mark_trace(
     concat_and_cache_mla, prefix="kv_cache", torch_compile=False
 )
