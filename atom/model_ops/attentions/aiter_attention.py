4b0b21d5cc4d36e79ac4c976ef3395c613629a9f
ShaoChunLee <Shao-Chun.Lee@amd.com>
2026-04-20 16:08:25 +0000

    address comment

--- atom/model_ops/attentions/aiter_attention.py
+++ atom/model_ops/attentions/aiter_attention.py
@@ -69,10 +69,10 @@ class AiterAttentionMetadataBuilder:
         self.block_size = 1024 if model_runner.block_size == 1024 else 16
         if envs.ATOM_ENABLE_TRITON_UNIFIED_ATTENTION_DECODE:
             # activate block_convertor when UA_decode is enabled
-            assert (
-                model_runner.block_size == 1024
-            ), "Please set --block-size to 1024 if ATOM_ENABLE_TRITON_UNIFIED_ATTENTION_DECODE=1"
             self.block_size = 128 if model_runner.kv_cache_dtype == "fp8" else 64
+        assert (
+            model_runner.block_size % self.block_size == 0
+        ), f"model_runner.block_size must be divisible by block_size but got {model_runner.block_size=}, block_size={self.block_size}, please set --block-size (model_runner.block_size) to be divisible by {self.block_size}"
         # Note: Cannot use super() here because the class is dynamically created by decorator
         # Use explicit parent class call instead
         CommonAttentionBuilder.__init__(self, model_runner)
