bcd173b101913d307ac9683338ebb24749757bfb
Wang Yiting <yitiwang@amd.com>
2026-06-11 09:34:56 +0800

    [Fea] Add PCP parallel

--- atom/model_engine/model_runner.py
+++ atom/model_engine/model_runner.py
@@ -801,7 +801,12 @@ class ModelRunner:
         # When data parallelism is enabled on the same node, different DP ranks
         # need to use different sets of GPUs
         dp_rank_local = config.parallel_config.data_parallel_rank_local or 0
-        local_device_rank = dp_rank_local * config.tensor_parallel_size + rank
+        local_device_rank = (
+            dp_rank_local
+            * config.tensor_parallel_size
+            * config.prefill_context_parallel_size
+            + rank
+        )
         num_gpus = torch.cuda.device_count()
         if local_device_rank >= num_gpus:
             raise ValueError(
@@ -828,6 +833,7 @@ class ModelRunner:
             distributed_init_method=distributed_init_method,
             data_parallel_size=config.parallel_config.data_parallel_size,
             data_parallel_rank=config.parallel_config.data_parallel_rank,
+            prefill_context_model_parallel_size=config.prefill_context_parallel_size,
         )
 
     def _make_buffer(
