9815abc59d742c1b38d59f9946e774efc7f009a2
Amelia Moore <Amelia.Moore@amd.com>
2026-06-11 18:20:11 +0000

    black

--- atom/model_ops/fused_moe_triton.py
+++ atom/model_ops/fused_moe_triton.py
@@ -38,12 +38,14 @@ if envs.ATOM_USE_TRITON_GEMM or envs.ATOM_USE_TRITON_MOE:
         mxfp4_quant,
     )
     from aiter.ops.triton.moe.quant_moe import downcast_to_static_fp8
-    from aiter.ops.triton.moe.moe_op_gemm_a4w4 import swizzle_scales  # same for a4 and a16
+    from aiter.ops.triton.moe.moe_op_gemm_a4w4 import (
+        swizzle_scales,
+    )  # same for a4 and a16
 
 
 def check_and_swizzle_scales(scale, N, K):
     # ensure swizzle is imported
-    assert envs.ATOM_USE_TRITON_GEMM or envs.ATOM_USE_TRITON_MOE 
+    assert envs.ATOM_USE_TRITON_GEMM or envs.ATOM_USE_TRITON_MOE
 
     if N % 32 == 0 and K % (32 * 8) == 0:
         scale = swizzle_scales(scale)
