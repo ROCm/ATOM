63b9809b4d1305ec13a904fccc60d9b2c57061a9
Amelia Moore <Amelia.Moore@amd.com>
2026-06-11 17:27:06 +0000

    ruff

--- atom/model_ops/moe_utils.py
+++ atom/model_ops/moe_utils.py
@@ -1,11 +1,4 @@
-from aiter.ops.triton.moe.quant_moe import downcast_to_mxfp
-from aiter.ops.triton.utils._triton.arch_info import get_arch
 from aiter.ops.triton.moe.moe_op_gemm_a4w4 import swizzle_scales  # same for a4 and a16
-import torch
-
-import logging
-
-logger = logging.getLogger("atom")  # debug
 
 
 def check_and_swizzle_scales(scale, N, K):
