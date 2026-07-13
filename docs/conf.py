613feee8918ca94d4b3110dc1f66ac819f4575da
Peter Park <peter.park@amd.com>
2026-07-12 23:47:58 -0400

    docs: update Sphinx config
    clean

--- docs/conf.py
+++ docs/conf.py
@@ -7,63 +7,52 @@ sys.path.insert(0, os.path.abspath(".."))
 
 # -- Project information -----------------------------------------------------
 project = "ATOM"
-copyright = "2026, AMD"
-author = "AMD ROCm Team"
-release = "0.1.0"
+copyright = "Copyright (c) %Y Advanced Micro Devices, Inc. All rights reserved."
+author = "Advanced Micro Devices, Inc."
+version = "0.1.0"
+release = version
 
 # -- General configuration ---------------------------------------------------
 extensions = [
-    "sphinx.ext.autodoc",
+    "rocm_docs",
+    # "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
     "sphinx.ext.viewcode",
-    "sphinx.ext.intersphinx",
-    "sphinx.ext.mathjax",
-    "sphinx_rtd_theme",
-    "myst_parser",
+    # "sphinx.ext.mathjax",
+    # "myst_parser",
 ]
+
+external_toc_path = "./sphinx/_toc.yml"
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
-source_suffix = {
-    ".rst": "restructuredtext",
-    ".md": "markdown",
-}
-
 # -- Options for HTML output -------------------------------------------------
-html_theme = "sphinx_rtd_theme"
-html_static_path = ["_static"]
-
+html_theme = "rocm_docs_theme"
 html_theme_options = {
-    "logo_only": False,
-    "display_version": True,
-    "prev_next_buttons_location": "bottom",
-    "style_external_links": False,
-    "style_nav_header_background": "#C00000",  # AMD Red
-    "collapse_navigation": False,
-    "sticky_navigation": True,
-    "navigation_depth": 4,
-    "includehidden": True,
-    "titles_only": False,
+    "flavor": "ai-ecosystem",
+    "link_main_doc": True,
+    "repository_url": "https://github.com/ROCm/ATOM",
+    "use_repository_button": True,
+    "use_issues_button": True,
 }
 
 html_logo = "assets/atom_logo.png"
-html_favicon = None
 
 # -- Extension configuration -------------------------------------------------
 
 # Napoleon settings
 napoleon_google_docstring = True
 napoleon_numpy_docstring = True
-
-# Intersphinx configuration
-intersphinx_mapping = {
-    "python": ("https://docs.python.org/3", None),
-    "torch": ("https://pytorch.org/docs/stable/", None),
-}
+#
+# # Intersphinx configuration
+# intersphinx_mapping = {
+#     "python": ("https://docs.python.org/3", None),
+#     "torch": ("https://pytorch.org/docs/stable/", None),
+# }
 
 # MyST parser settings
-myst_enable_extensions = [
+myst_enable_extensions = {
     "colon_fence",
     "deflist",
-]
+}
